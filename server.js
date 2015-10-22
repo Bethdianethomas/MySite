3cc9b980e4a816d7472baee5e00ef4260bb0e906
bethdianethomas <bethdianethomas@yahoo.com>
2015-10-22 09:39:07 -0600

    morning git commit -m morning

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ var path = require('path');
 var http = require('http');
 var fs = require('fs');
 var bodyParser = require('body-parser');
-var db = require('./model/db');
+// var db = require('./model/db');
 var blogModel = require('./model/blog');
 
 var app = express();
 
-var blogRoutes = require('./routes/blog');
+// var blogRoutes = require('./routes/blog');
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static('public'));
 
-app.use('/api/blog', blogRoutes)
+// app.use('/api/blog', blogRoutes)
 
 app.get('/', function(req, res){
     res.readFile('index.html');
